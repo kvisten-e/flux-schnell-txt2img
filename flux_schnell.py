fe07707aa7b11ab43b33613dd9bbde7bce9b7848
burak <byrkbrk@gmail.com>
2024-08-17 21:06:37 +0300

    update FluxSchnell class

--- flux_schnell.py
+++ flux_schnell.py
@@ -1,13 +1,45 @@
 import os
+import torch
 from diffusers import DiffusionPipeline
 
 
 class FluxSchnell:
-    def __init__(self):
-        self.model = DiffusionPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell")
+    repo_id = "black-forest-labs/FLUX.1-schnell"
+    
+    def __init__(self,
+                 device: str = "mps",
+                 create_dirs: bool = True):
+        self.module_dir = os.path.dirname(__file__)
+        self.device = self.initialize_device(device)
+        self.model = self.instantiate_model(self.__class__.repo_id)
+        if create_dirs: self.create_dirs(self.module_dir)
 
-    def generate(self, prompt):
-        pass
+    def generate(self, prompt, save=True, show=True):
+        """Returns list of generated images for given prompts"""
+        images = self.model(prompt)
+        for i, image in enumerate(images):
+            if save:
+                image.save(os.path.join(self.module_dir, 
+                                        "generated-images",
+                                        f"generated_image_{i}.png"))
+            if show:
+                image.show()
+        return images
+
+    def instantiate_model(self, repo_id):
+        """Returns instantiated model"""
+        return DiffusionPipeline.from_pretrained(pretrained_model_name_or_path=repo_id)
+    
+    def initialize_device(self, device: str):
+        """Return the GPU device based on availability"""
+        return torch.device(device)
+
+    def create_dirs(self, root):
+        """Creates required directories under given root directory"""
+        dir_names = ["generated-images"]
+        for dir_name in dir_names:
+            os.makedirs(os.path.join(root, dir_name), exist_ok=True)
+
 
 
 if __name__ == "__main__":
