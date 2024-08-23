421e427ba55b9f81c6f0b3fb6a33954124de65ca
burak <byrkbrk@gmail.com>
2024-08-23 16:53:31 +0300

    add parse_arguments function

--- generate.py
+++ generate.py
@@ -1,2 +1,27 @@
 from argparse import ArgumentParser
 from flux_schnell import FluxSchnell
+
+
+
+def parse_arguments():
+    """Returns parsed arguments"""
+    parser = ArgumentParser(description="Generate images by prompt using Flux-schnell")
+    parser.add_argument("prompt", 
+                        type=str, 
+                        nargs="+",
+                        help="Prompt that be used during inference")
+    parser.add_argument("--device", 
+                        type=str,
+                        default=None,
+                        choices=["cuda", "mps", "cpu"],
+                        help="The device used during inference. Default: `None`")
+    parser.add_argument("--enable_sequential_cpu_offload",
+                        action="store_true",
+                        help="Enables sequentail cpu offload during inference")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    for arg, value in vars(args).items():
+        print(f"{arg}: {value}")
