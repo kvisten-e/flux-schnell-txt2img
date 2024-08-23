8ecf6eb9aa95d15669841b5fb29c3d2bdfb0292f
burak <byrkbrk@gmail.com>
2024-08-23 17:14:28 +0300

    update generate.py file

--- generate.py
+++ generate.py
@@ -10,6 +10,10 @@ def parse_arguments():
                         type=str, 
                         nargs="+",
                         help="Prompt that be used during inference")
+    parser.add_argument("--num_inference_steps",
+                        type=int,
+                        default=4,
+                        help="Number of inference steps used during generating")
     parser.add_argument("--device", 
                         type=str,
                         default=None,
@@ -17,11 +21,14 @@ def parse_arguments():
                         help="The device used during inference. Default: `None`")
     parser.add_argument("--enable_sequential_cpu_offload",
                         action="store_true",
-                        help="Enables sequentail cpu offload during inference")
+                        help="Enables sequential cpu offload during inference")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    for arg, value in vars(args).items():
-        print(f"{arg}: {value}")
+    FluxSchnell(
+        device=args.device,
+        enable_sequential_cpu_offload=args.enable_sequential_cpu_offload
+    ).generate(args.prompt,
+               args.num_inference_steps)
